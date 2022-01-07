1194aecd5363d8f6ba5b5befd7acbd68e2e8a5cd
Abhishek Kumar <ak00873@gmail.com>
2022-01-07 11:15:44 +0530

    Subtraction of product and sum of given n number

---
+++ subtraction_of_product_sum.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int sub(int n)
+{
+    int sum=0;
+    int product=1;
+    while(n>0)
+    {
+        sum+=n%10;
+        product*=n%10;
+        n/=10;
+    }
+    printf("\nsum=%d\nproduct=%d\n",sum,product);
+    return product-sum;
+}
+
+int main()
+{
+    int num,d;
+
+    printf("Enter No \n");
+    scanf("%d",&num);
+
+    d=sub(num);
+     
+    printf("Difference of Product and Sum=%d\n",d);
+}
